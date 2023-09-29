bfa83e4d35e56d1de31806f1a272ac26e0dd6d0c
5_keith <godlazy0@gmail.com>
2023-09-29 23:28:02 +0530

    Installer Improvements

--- Server/CLI/setup.py
+++ Server/CLI/setup.py
@@ -1,15 +1,41 @@
 from cx_Freeze import setup, Executable
 
 includefiles = ['spec/ViGEmClient.dll']
+#Literally copied from https://stackoverflow.com/questions/15734703/use-cx-freeze-to-create-an-msi-that-adds-a-shortcut-to-the-desktop
+shortcut_table = [
+    ("StartMenuFolder",        # Shortcut
+     "DesktopFolder",          # Directory_
+     "Playpulse Link",           # Name that will be show on the link
+     "TARGETDIR",              # Component_
+     "[TARGETDIR]Playpulse Desktop.exe",# Target exe to exexute
+     None,                     # Arguments
+     None,                     # Description
+     None,                     # Hotkey
+     None,                     # Icon
+     None,                     # IconIndex
+     None,                     # ShowCmd
+     'TARGETDIR'               # WkDir
+     )
+    ]
+# Now create the table dictionary
+msi_data = {"Shortcut": shortcut_table}
 
+# Change some default MSI options and specify the use of the above defined tables
+bdist_msi_options = {'data': msi_data}
+
+options = {
+    "bdist_msi": bdist_msi_options,
+    'build_exe': {
+                'include_files': includefiles
+                }
+}
 setup(
     name='Playpulse Desktop',
     version='1.0',
     description='Turn your smartphone into a game controller with this simple app.',
     author='zan-keith',
-    options={'build_exe': {'include_files': includefiles}},
+    options=options,
     executables=[Executable('main.py',
                             target_name="Playpulse Desktop.exe",
-                            icon="darkicon.ico",
-                            shortcut_name="Playpulse")]
+                            icon="darkicon.ico",)]
 )
--- Server/GUI/setup.py
+++ Server/GUI/setup.py
@@ -1,16 +1,46 @@
-from cx_Freeze import setup, Executable
+from cx_Freeze import setup, Executable, bdist_msi
 
-includefiles = ['spec/ViGEmClient.dll','driver_fix.py',('assets/')]
+includefiles = ['spec/ViGEmClient.dll', 'driver_fix.py', ('assets/')]
+
+#Literally copied from https://stackoverflow.com/questions/15734703/use-cx-freeze-to-create-an-msi-that-adds-a-shortcut-to-the-desktop
+shortcut_table = [
+    ("StartMenuFolder",        # Shortcut
+     "DesktopFolder",          # Directory_
+     "Playpulse Link",           # Name that will be show on the link
+     "TARGETDIR",              # Component_
+     "[TARGETDIR]Playpulse Desktop.exe",# Target exe to exexute
+     None,                     # Arguments
+     None,                     # Description
+     None,                     # Hotkey
+     None,                     # Icon
+     None,                     # IconIndex
+     None,                     # ShowCmd
+     'TARGETDIR'               # WkDir
+     )
+    ]
+# Now create the table dictionary
+msi_data = {"Shortcut": shortcut_table}
+
+# Change some default MSI options and specify the use of the above defined tables
+bdist_msi_options = {'data': msi_data}
+
+options = {
+    "bdist_msi": bdist_msi_options,
+    'build_exe': {
+                'include_files': includefiles
+                }
+}
 
 setup(
     name='Playpulse Desktop GUI',
     version='1.0',
     description='Turn your smartphone into a game controller with this simple app.',
     author='zan-keith',
-    options={'build_exe': {'include_files': includefiles}},
     executables=[Executable('main.py',
                             target_name="Playpulse Desktop.exe",
                             icon="lighticon.ico",
-                            base = "Win32GUI",
-                            shortcut_name="Playpulse")]
+                            base="Win32GUI",
+                            )],
+    options=options,
+
 )
